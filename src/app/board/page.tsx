d01134ab4d8e370f253190eaa464c6c1a37b3d53
Willow <isaiasnoel@gmail.com>
2025-09-30 10:39:46 -0700

    small fix and board page imports

--- src/app/board/page.tsx
+++ src/app/board/page.tsx
@@ -1,5 +1,15 @@
+import MeetTheTeam from "@/components/board/MeetTheTeam";
+import Header from "@/components/Header";
+import BoardCard from "@/components/board/BoardCard";
+
 const Board = () => {
-  return <div>Board</div>;
+  return (
+    <div className="flex h-screen w-screen items-center justify-center">
+      <MeetTheTeam />
+      <Header />
+      <BoardCard />
+    </div>
+  )
 };
 
 export default Board;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-import Header from "@/components/Header";
 import AboutUs from "@/components/home/AboutUs";
 import OurTeam from "@/components/home/OurTeam";
 import Carousel from "@/components/home/LogoCarousel";
@@ -8,7 +7,6 @@ import MailingList from "@/components/home/MailingList";
 const Home = () => {
   return (
     <div className="flex h-screen w-screen items-center justify-center">
-      <Header />
       <AboutUs />
       <OurTeam />
       <Carousel />
