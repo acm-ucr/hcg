366ec9ebda92448a4decf63bd23f272f792b6bad
Willow <isaiasnoel@gmail.com>
2025-09-30 10:50:49 -0700

    alumni page imports

--- src/app/alumni/page.tsx
+++ src/app/alumni/page.tsx
@@ -1,5 +1,15 @@
+import AlumniHeader from "@/components/alumni/Alumni";
+import Header from "@/components/Header";
+import AlumniMapping from "@/components/alumni/AlumniMapping";
+
 const Alumni = () => {
-  return <div>Alumni</div>;
+  return (
+  <div>
+    <AlumniHeader />
+    <Header />
+    <AlumniMapping />
+  </div>
+  )
 };
 
 export default Alumni;
