bcd6528ef5d765eacc09d9daf8ef4a947db6c7b0
Willow <isaiasnoel@gmail.com>
2025-09-30 10:19:53 -0700

    home page.tsx functions imported

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,19 @@
+import Header from "@/components/Header";
+import AboutUs from "@/components/home/AboutUs";
+import OurTeam from "@/components/home/OurTeam";
+import Carousel from "@/components/home/LogoCarousel";
+import Clients from "@/components/home/Clients";
+import MailingList from "@/components/home/MailingList";
+
 const Home = () => {
   return (
     <div className="flex h-screen w-screen items-center justify-center">
-      Hello World
+      <Header />
+      <AboutUs />
+      <OurTeam />
+      <Carousel />
+      <Clients />
+      <MailingList />
     </div>
   );
 };
