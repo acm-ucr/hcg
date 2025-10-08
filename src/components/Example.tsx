d66b39773465a9dba2d4989b1ee13400040d5ac3
kitty1737 <obesedoge@gmail.com>
2025-10-07 20:59:47 -0700

    Rounded outer and 2 inner boxes

--- src/components/Example.tsx
+++ src/components/Example.tsx
@@ -1,11 +1,11 @@
 const Example = (props: { text1: string; text2: string; text3: string }) => {
   return (
-    <div className="bg-hcg-gold flex w-2/3 flex-col items-center gap-1 justify-self-center p-8">
+    <div className="bg-hcg-gold flex w-2/3 flex-col items-center gap-1 justify-self-center p-8 rounded-sm">
         <div className="bg-hcg-pastel-brown w-1/2 p-5 text-left">{props.text1}</div>
 
         <div className="flex w-1/2 flex-row gap-1">
-            <div className="bg-hcg-medium-brown w-full p-5 text-left">props.text2</div>
-            <div className="bg-hcg-pastel-brown w-full p-5 text-right">{props.text2}</div>
+            <div className="bg-hcg-medium-brown w-full p-5 text-left rounded-lg">props.text2</div>
+            <div className="bg-hcg-pastel-brown w-full p-5 text-right rounded-lg">{props.text2}</div>
         </div>
 
         <div className="bg-hcg-medium-brown w-1/2 p-5 text-left">Test</div>
