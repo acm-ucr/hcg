9082c51fe8dc883131df4166fb2a6afb7c4f3fa6
kitty1737 <obesedoge@gmail.com>
2025-10-07 21:26:55 -0700

    Adjusted text for fourth div to appropriate value

--- src/components/Example.tsx
+++ src/components/Example.tsx
@@ -1,14 +1,14 @@
 const Example = (props: { text1: string; text2: string; text3: string }) => {
   return (
     <div className="bg-hcg-gold flex w-2/3 flex-col items-center gap-1 justify-self-center p-8 rounded-sm text-lg">
-        <div className="bg-hcg-pastel-brown w-1/2 p-5 text-left mb-3">{props.text1}</div>
+        <div className="bg-hcg-pastel-brown w-1/2 p-5 text-center mb-3">{props.text1}</div>
 
         <div className="flex w-1/2 flex-row gap-1 mb-3">
-            <div className="bg-hcg-medium-brown w-full p-5 text-left rounded-lg">props.text2</div>
-            <div className="bg-hcg-pastel-brown w-full p-5 text-right rounded-lg">{props.text2}</div>
+            <div className="bg-hcg-medium-brown w-full p-5 text-center rounded-lg">{props.text2}</div>
+            <div className="bg-hcg-pastel-brown w-full p-5 text-center rounded-lg">{props.text2}</div>
         </div>
 
-        <div className="bg-hcg-medium-brown w-1/2 p-5 text-left text-white">Props</div>
+          <div className="bg-hcg-medium-brown w-1/2 p-5 text-center text-white">{props.text3}</div>
     </div>
   );
 };
