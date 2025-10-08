12ae29172b258ce7e1f2d955a3b112c9a66b7e91
kitty1737 <obesedoge@gmail.com>
2025-10-07 20:50:26 -0700

    Changed width of outer box to 2/3 from 3/4

--- src/components/Example.tsx
+++ src/components/Example.tsx
@@ -1,6 +1,6 @@
 const Example = (props: { text1: string; text2: string; text3: string }) => {
   return (
-    <div className="bg-hcg-gold flex w-3/4 flex-col items-center gap-1 justify-self-center p-8">
+    <div className="bg-hcg-gold flex w-2/3 flex-col items-center gap-1 justify-self-center p-8">
         <div className="bg-hcg-white w-1/2 p-5 text-left">{props.text1}</div>
 
         <div className="flex w-1/2 flex-row gap-1">
