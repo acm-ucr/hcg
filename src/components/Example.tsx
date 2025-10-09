328fcdcbec434f706cc636fb3c01707f92aa4aec
kitty1737 <obesedoge@gmail.com>
2025-10-08 23:18:29 -0700

    Ran prettier on example.tsx

--- src/components/Example.tsx
+++ src/components/Example.tsx
@@ -1,14 +1,22 @@
 const Example = (props: { text1: string; text2: string; text3: string }) => {
   return (
-    <div className="bg-hcg-gold flex w-2/3 flex-col items-center gap-1 justify-self-center p-8 rounded-sm text-lg">
-        <div className="bg-hcg-pastel-brown w-1/2 p-5 text-center mb-4">{props.text1}</div>
+    <div className="bg-hcg-gold flex w-2/3 flex-col items-center gap-1 justify-self-center rounded-sm p-8 text-lg">
+      <div className="bg-hcg-pastel-brown mb-4 w-1/2 p-5 text-center">
+        {props.text1}
+      </div>
 
-        <div className="flex w-1/2 flex-row gap-1 mb-4">
-            <div className="bg-hcg-medium-brown w-full p-5 text-center rounded-lg">{props.text2}</div>
-            <div className="bg-hcg-pastel-brown w-full p-5 text-center rounded-lg">{props.text2}</div>
+      <div className="mb-4 flex w-1/2 flex-row gap-1">
+        <div className="bg-hcg-medium-brown w-full rounded-lg p-5 text-center">
+          {props.text2}
         </div>
+        <div className="bg-hcg-pastel-brown w-full rounded-lg p-5 text-center">
+          {props.text2}
+        </div>
+      </div>
 
-          <div className="bg-hcg-medium-brown w-1/2 p-5 text-center text-white">{props.text3}</div>
+      <div className="bg-hcg-medium-brown w-1/2 p-5 text-center text-white">
+        {props.text3}
+      </div>
     </div>
   );
 };
