6dfc01007efee3897d506450cd4ed2bb007c83a1
kitty1737 <obesedoge@gmail.com>
2025-10-08 23:15:05 -0700

    Made margin a bit bigger

--- src/components/Example.tsx
+++ src/components/Example.tsx
@@ -1,9 +1,9 @@
 const Example = (props: { text1: string; text2: string; text3: string }) => {
   return (
     <div className="bg-hcg-gold flex w-2/3 flex-col items-center gap-1 justify-self-center p-8 rounded-sm text-lg">
-        <div className="bg-hcg-pastel-brown w-1/2 p-5 text-center mb-3">{props.text1}</div>
+        <div className="bg-hcg-pastel-brown w-1/2 p-5 text-center mb-4">{props.text1}</div>
 
-        <div className="flex w-1/2 flex-row gap-1 mb-3">
+        <div className="flex w-1/2 flex-row gap-1 mb-4">
             <div className="bg-hcg-medium-brown w-full p-5 text-center rounded-lg">{props.text2}</div>
             <div className="bg-hcg-pastel-brown w-full p-5 text-center rounded-lg">{props.text2}</div>
         </div>
