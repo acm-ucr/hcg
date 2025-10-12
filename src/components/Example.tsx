b58462388751331af218343f4f01649e58e12274
APerson09 <ethanhuang63@gmail.com>
2025-10-12 15:16:00 -0700

    Made example Component

--- src/components/Example.tsx
+++ src/components/Example.tsx
@@ -1,12 +1,20 @@
 const Example = (props: { text1: string; text2: string; text3: string }) => {
   return (
-    <div className="bg-hcg-gold flex w-3/4 items-center gap-1 justify-self-center p-8">
-      <div className="bg-hcg-white w-1/2 p-5 text-left">{props.text1}</div>
-      <div className="flex w-1/2 flex-row gap-1">
-        <div className="bg-hcg-white w-full p-5 text-left">props.text2</div>
-        <div className="bg-hcg-white w-full p-5 text-right">{props.text2}</div>
+    <div className="bg-hcg-gold flex w-2/3 flex-col items-center gap-1 justify-self-center p-8">
+      <div className="bg-hcg-pastel-brown m-5 w-2/3 p-5 text-center text-lg">
+        {props.text1}
       </div>
-      {/* Hint: Add another box/div here similarly to line 4 */}
+      <div className="flex w-2/3 flex-row gap-1">
+        <div className="bg-hcg-medium-brown w-full rounded-xl p-5 text-center text-lg">
+          {props.text2}
+        </div>
+        <div className="bg-hcg-pastel-brown w-full rounded-xl p-5 text-center text-lg">
+          {props.text2}
+        </div>
+      </div>
+      <div className="bg-hcg-medium-brown m-5 w-2/3 items-center gap-1 justify-self-center p-5 text-center text-lg text-white">
+        Props
+      </div>
     </div>
   );
 };
