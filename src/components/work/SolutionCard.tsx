58763378bd47edab5f62d5ca4b6874cc24aaacc3
kitty1737 <obesedoge@gmail.com>
2025-10-18 14:52:58 -0700

    ran prettier on solution card component

--- src/components/work/SolutionCard.tsx
+++ src/components/work/SolutionCard.tsx
@@ -1,25 +1,25 @@
 import Image, { StaticImageData } from "next/image";
 interface SolutionCardProps {
-	cardTitle: string,
-	cardText: string,
-	imageSrc: StaticImageData
+  cardTitle: string;
+  cardText: string;
+  imageSrc: StaticImageData;
 }
 
-const SolutionCard = ({cardTitle, cardText, imageSrc}: SolutionCardProps) => {
-	return (
-		<div className="max-w-lg bg-[#bfa57f] p-5 rounded-lg">
-			<div className= "flex flex-col items-center text-center p-5 space-y-10 text-white rounded-lg border-3 border-white">
-				<Image
-					src={imageSrc}
-					alt={"Image of " + cardTitle}
-					width={60}
-					height={60}
-				/>
-				<h1 className="font-bold text-3xl">{cardTitle}</h1>
-				<p className="text-xl mb-5">{cardText}</p>
-			</div>
-		</div>
-	);
+const SolutionCard = ({ cardTitle, cardText, imageSrc }: SolutionCardProps) => {
+  return (
+    <div className="max-w-lg rounded-lg bg-[#bfa57f] p-5">
+      <div className="flex flex-col items-center space-y-10 rounded-lg border-3 border-white p-5 text-center text-white">
+        <Image
+          src={imageSrc}
+          alt={"Image of " + cardTitle}
+          width={60}
+          height={60}
+        />
+        <h1 className="text-3xl font-bold">{cardTitle}</h1>
+        <p className="mb-5 text-xl">{cardText}</p>
+      </div>
+    </div>
+  );
 };
 
 export default SolutionCard;
