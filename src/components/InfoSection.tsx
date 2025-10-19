2fa146c6b19ad3976ba9537b107132c0079384ad
Jason <bennguyen20122006@gmail.com>
2025-10-18 22:02:34 -0700

    Modified changes based on feedback

--- src/components/InfoSection.tsx
+++ src/components/InfoSection.tsx
@@ -1,19 +1,27 @@
-import { InfoSectionProps } from "@/utils/types";
 import Image from "next/image";
+
+import { StaticImageData } from "next/image";
+
+export interface InfoSectionProps {
+  title: string;
+  text: string;
+  image: StaticImageData;
+  imageAlt: string;
+}
 
 const InfoSection = ({ title, text, image, imageAlt }: InfoSectionProps) => {
   return (
-    <div className="mx-auto flex min-h-[90vh] w-9/10 flex-row items-center justify-center">
+    <div className="mx-auto flex w-5/6 flex-row items-center justify-center py-8">
       <div className="flex w-1/2 flex-col justify-center space-y-6">
-        <h2 className="text-5xl leading-relaxed font-[700] text-[#7A6A58]">
+        <div className="text-5xl font-bold text-(--color-hcg-pastel-brown)">
           {title}
-        </h2>
-        <p className="flex w-3/4 items-center justify-center text-2xl font-[400]">
+        </div>
+        <p className="flex w-3/4 items-center justify-center text-2xl font-normal">
           {text}
         </p>
       </div>
-      <div className="align-center flex h-auto w-1/2 justify-center">
-        <Image src={image} alt={imageAlt} className="h-2xl w-2xl" />
+      <div className="flex h-auto w-1/2 justify-center">
+        <Image src={image} alt={imageAlt} />
       </div>
     </div>
   );
--- src/utils/types.d.ts
+++
@@ -1,8 +0,0 @@
-import { StaticImageData } from "next/image";
-
-export interface InfoSectionProps {
-  title: string;
-  text: string;
-  image: StaticImageData;
-  imageAlt: string;
-}
