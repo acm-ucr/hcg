180752b65eb2b12b9e27079632127cf7e9b33879
Celina Vasquez <cvasq043@ucr.edu>
2025-10-19 21:21:04 -0700

    Added image and text to header across multiple pages

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,21 @@
+import Image from "next/image";
+import StaticImageData from "@/public/work/clientWorkBG.webp";
+
 const Header = () => {
-  return <div>Header</div>;
+  const textStyle = {
+    position: "absolute",
+    top: "75%",
+    left: "36.5%",
+    color: "white",
+    fontSize: "35pt",
+  };
+
+  return (
+    <div>
+      <Image src={StaticImageData} alt="General Header"></Image>
+      <div style={textStyle}>Meet the Team</div>
+    </div>
+  );
 };
 
 export default Header;
