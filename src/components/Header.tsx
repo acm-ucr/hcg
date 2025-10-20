f517fad2c55d105c75c5dd5d2be6b0c1a89b5e37
Celina Vasquez <cvasq043@ucr.edu>
2025-10-19 22:48:54 -0700

    Added image and text to header across multiple pages

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,7 +6,9 @@ const Header = () => {
     <div>
       <Image src={StaticImageData} alt="General Header" />
       <div className="absolute inset-0 flex items-center justify-center">
-        <h1 className="text-white w-1/2 p-5 text-center text-4xl">Meet the Team</h1>
+        <h1 className="w-1/2 p-5 text-center text-4xl text-white">
+          Meet the Team
+        </h1>
       </div>
     </div>
   );
