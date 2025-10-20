4edbf868fb7e04c17172073d9b686ff30d5e026a
mealthebear <mealthebear@gmail.com>
2025-10-20 15:49:49 -0700

    Added landing page image and overlay text
    * Used Nextjs Image component as well as flexbox styles to overlay text on top of image

--- src/components/home/LandingHeader.tsx
+++ src/components/home/LandingHeader.tsx
@@ -1,5 +1,19 @@
+import LandingPageLogo from "@/public/mainLogo.webp";
+import Image from "next/image";
+
 const LandingHeader = () => {
-  return <div>LandingHeader</div>;
+  return (
+    <section className="flex flex-col items-center justify-center">
+      <Image
+        src={LandingPageLogo}
+        alt="Higherlander Consulting Group Club Members"
+      />
+      <div className="absolute content-center text-center">
+        <h2 className="text-3xl">Highlander Consulting Group</h2>
+        <p>{`UC Riverside's Premier Consulting Organization`}</p>
+      </div>
+    </section>
+  );
 };
 
 export default LandingHeader;
