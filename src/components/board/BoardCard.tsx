c761dfb68cd79645dfc70f8eae665037cec762f8
Tarunvir <gtarunvir@gmail.com>
2025-10-21 11:25:11 -0700

    Applied fixes to boardcard

--- src/components/board/BoardCard.tsx
+++ src/components/board/BoardCard.tsx
@@ -7,27 +7,22 @@ export interface BoardCardProps {
   role: string;
   image: StaticImageData;
   linkedin: string;
-
 }
 
-const BoardCard = ({name, role, image, linkedin}: BoardCardProps) => {
-  return(
-   
-   <div className="rounded-lg p-6 flex flex-col items-center">
-        <Image src={image} alt={name}  className="rounded-full mx-auto mb-4" />
-      
-      <div className="text-xl font-semibold text-center">{name}</div>
+const BoardCard = ({ name, role, image, linkedin }: BoardCardProps) => {
+  return (
+    <div className="flex flex-col items-center rounded-lg p-6">
+      <Image src={image} alt={name} className="mx-auto mb-4 rounded-full" />
 
-      <div className="text-gray-600 text-center mb-4">{role}</div>
+      <div className="text-center text-xl font-semibold">{name}</div>
+
+      <div className="mb-4 text-center text-gray-600">{role}</div>
 
       <Link href={linkedin} target="_blank">
-        <FaLinkedin className="text-hcg-gold width:40px height:40px"/>
+        <FaLinkedin className="text-hcg-gold h-8 w-8" />
       </Link>
-</div>
+    </div>
   );
 };
 
-
 export default BoardCard;
-
-
