ae5890c269a04b3e5bb78c70fb0e55275f505b8f
mealthebear <mealthebear@gmail.com>
2025-10-20 17:27:16 -0700

    Changed html elements to divs for consistency
    * For this project, divs are mostly used as containers to make the code look similar and maintain consistency

--- src/components/home/LandingHeader.tsx
+++ src/components/home/LandingHeader.tsx
@@ -3,16 +3,16 @@ import Image from "next/image";
 
 const LandingHeader = () => {
   return (
-    <section className="flex flex-col items-center justify-center">
+    <div className="flex flex-col items-center justify-center">
       <Image
         src={LandingPageLogo}
         alt="Higherlander Consulting Group Club Members"
       />
       <div className="text-hcg-white absolute content-center text-center">
-        <h2 className="text-3xl font-bold">Highlander Consulting Group</h2>
-        <p>{`UC Riverside's Premier Consulting Organization`}</p>
+        <div className="text-3xl font-bold">Highlander Consulting Group</div>
+        <div>{`UC Riverside's Premier Consulting Organization`}</div>
       </div>
-    </section>
+    </div>
   );
 };
 
