0be29e90a5029a6cf9d724f2b6e0ff6d9eb7c210
Jason <bennguyen20122006@gmail.com>
2025-10-21 10:28:41 -0700

    Created Error Message Component

--- src/components/ErrorMessage.tsx
+++ src/components/ErrorMessage.tsx
@@ -1,5 +1,15 @@
+import Button from "@/components/Button";
+
 const ErrorMessage = () => {
-  return <div>Error</div>;
+  return (
+    <div className="flex flex-col items-center justify-center p-6 text-center">
+      <div className="text-[120px] font-bold text-(--color-hcg-pastel-brown) sm:text-[300px]">
+        404
+      </div>
+      <div className="p-6 text-2xl sm:text-3xl">Page Not Found</div>
+      <Button text="Back To Home" href="/" />
+    </div>
+  );
 };
 
 export default ErrorMessage;
