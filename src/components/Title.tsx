6d867f3a1f8bc9259e3c334028dc0d7f6cd0d5c9
Celina Vasquez <cvasq043@ucr.edu>
2025-10-27 01:14:11 -0700

    Added board/alumni title using a component

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,5 +1,13 @@
-const Title = () => {
-  return <div>Title</div>;
+interface titleProps {
+  title: string;
+}
+
+const Title = ({ title }: titleProps) => {
+  return (
+    <div className="text-hcg-pastel-brown translate-y-3/4 text-center text-5xl font-medium">
+      {title}
+    </div>
+  );
 };
 
 export default Title;
