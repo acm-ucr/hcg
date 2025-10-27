2c5a17d5281db23cb5d446a9d69595e3cd5c1f87
LawMot <molawr12@gmail.com>
2025-10-27 15:55:34 -0700

    create carousel

--- src/components/home/LogoCarousel.tsx
+++ src/components/home/LogoCarousel.tsx
@@ -1,5 +1,27 @@
+"use client";
+
+import { carouselData } from "@/data/CarouselItems";
+import Image from "next/image";
+
 const LogoCarousel = () => {
-  return <div>LogoCarousel</div>;
+  return (
+    <div className="bg-hcg-white w-full overflow-hidden">
+      <div className="animate-scroll flex items-center">
+        {carouselData.map(({ name, icon }, idx) => (
+          <div
+            key={idx}
+            className="bg-hcg-white flex items-center justify-center px-4 py-4"
+          >
+            <Image
+              src={icon}
+              alt={`${name} logo`}
+              className="w-auto object-contain"
+            />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default LogoCarousel;
