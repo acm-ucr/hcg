1a125ce618905a62a9a2c55e5af568725ce3c4f8
Willow <isaiasnoel@gmail.com>
2025-11-06 13:14:17 -0800

    formatting and layout font size adjustment

--- src/components/home/MailingList.tsx
+++ src/components/home/MailingList.tsx
@@ -1,28 +1,28 @@
 const MailingList = () => {
   return (
-    <div className="flex items-center justify-center min-h-screen">
-      <div className="bg-hcg-gold rounded-2xl p-2 w-4/5">
-        <div className="border border-white rounded-2xl p-10 font-hcg-main font-light">
-          <div className="text-center text-2xl md:text-5xl font-bold text-white mb-10">
+    <div className="flex min-h-screen items-center justify-center">
+      <div className="bg-hcg-gold w-4/5 rounded-2xl p-2">
+        <div className="font-hcg-main rounded-2xl border border-white p-10 font-light">
+          <div className="mb-10 text-center text-2xl font-bold text-white md:text-5xl">
             Join our Mailing List
           </div>
           <form className="space-y-6">
-            <div className="flex flex-col md:flex-row md:space-x-6 space-y-6 md:space-y-0">
+            <div className="flex flex-col space-y-6 md:flex-row md:space-y-0 md:space-x-6">
               <div className="flex-1">
-                <div className="text-white mb-2 text-lg">First Name</div>
-                <input className="w-full p-3 bg-white focus:outline-none" />
+                <div className="mb-2 text-lg text-white">First Name</div>
+                <input className="w-full bg-white p-3 focus:outline-none" />
               </div>
-              <div className="flex-1 hidden md:block">
-                <div className="text-white mb-2 text-lg">Last Name</div>
-                <input className="w-full p-3 bg-white focus:outline-none" />
+              <div className="hidden flex-1 md:block">
+                <div className="mb-2 text-lg text-white">Last Name</div>
+                <input className="w-full bg-white p-3 focus:outline-none" />
               </div>
             </div>
             <div>
-              <div className="text-white mb-2 text-lg">Email</div>
-              <input className="w-full p-3 bg-white focus:outline-none"/>
+              <div className="mb-2 text-lg text-white">Email</div>
+              <input className="w-full bg-white p-3 focus:outline-none" />
             </div>
-            <div className="flex justify-center md:justify-start mt-6">
-              <button className="bg-black text-lg text-white px-8 py-3 rounded-lg cursor-pointer">
+            <div className="mt-6 flex justify-center md:justify-start">
+              <button className="cursor-pointer rounded-lg bg-black px-8 py-3 text-lg text-white">
                 Submit
               </button>
             </div>
