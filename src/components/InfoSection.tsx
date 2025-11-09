0910f728ae98d5e088d8e8b9a7b6f66060ad3d46
kitty1737 <obesedoge@gmail.com>
2025-11-08 23:34:10 -0800

    Now using Title component

--- src/components/InfoSection.tsx
+++ src/components/InfoSection.tsx
@@ -1,4 +1,5 @@
 import Image, { StaticImageData } from "next/image";
+import Title from "@/components/Title"
 
 export interface InfoSectionProps {
   title: string;
@@ -12,9 +13,7 @@ const InfoSection = ({ title, text, image, imageAlt }: InfoSectionProps) => {
     <div>
       <div className="mx-auto flex w-5/6 flex-row items-center justify-center py-4 md:py-8">
         <div className="flex flex-col justify-center space-y-3 md:w-1/2 md:space-y-6">
-          <div className="text-hcg-pastel-brown med:text-3xl text-center text-xl font-bold sm:text-2xl md:text-left">
-            {title}
-          </div>
+          <Title title={title} color={"text-hcg-pastel-brown"} className={"med:text-3xl text-center text-xl font-bold sm:text-2xl md:text-left"} />
           <p className="flex items-center justify-center text-center text-sm font-normal sm:text-lg md:w-3/4 md:text-left md:text-xl">
             {text}
           </p>
