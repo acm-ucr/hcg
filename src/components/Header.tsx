379ec468e76fc82c160015144a85fa13802ddd35
alimtiaco0 <alimtiaco0@saddleback.edu>
2025-11-10 02:32:08 -0800

    Fixed responsive header changes

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,7 +7,7 @@ interface HeaderProps {
 
 const Header = ({ title, headerPicture }: HeaderProps) => {
   return (
-    <div className="relative h-[28vh] w-full sm:h-[32vh] md:h-[38vh] lg:h-[46vh] xl:h-[52vh]">
+    <div className="relative h-[28vh] w-full md:h-[38vh] xl:h-[52vh]">
       <Image
         src={headerPicture}
         alt="General Header"
@@ -15,7 +15,7 @@ const Header = ({ title, headerPicture }: HeaderProps) => {
         className="object-cover object-top opacity-60"
         priority
       />
-      <div className="absolute top-1/2 w-full -translate-y-1/2 p-5 text-center text-3xl font-medium text-white md:text-5xl lg:text-6xl">
+      <div className="absolute top-1/2 w-full -translate-y-1/2 p-5 text-center text-3xl font-medium text-white md:text-5xl xl:text-6xl">
         {title}
       </div>
     </div>
