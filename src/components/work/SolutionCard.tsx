bbde34be9eaa24c6f2dc0510390960034a6560e1
kitty1737 <obesedoge@gmail.com>
2025-11-09 19:08:29 -0800

    Added animations for Solution.

--- src/components/work/SolutionCard.tsx
+++ src/components/work/SolutionCard.tsx
@@ -1,20 +1,28 @@
+"use client"
+
 import Image, { StaticImageData } from "next/image";
+import { motion } from "motion/react";
+
 interface SolutionCardProps {
-  cardTitle: string;
-  cardText: string;
-  imageSrc: StaticImageData;
+    cardTitle: string;
+    cardText: string;
+    imageSrc: StaticImageData;
 }
 
 const SolutionCard = ({ cardTitle, cardText, imageSrc }: SolutionCardProps) => {
-  return (
-    <div className="bg-hcg-pastel-brown-secondary mx-auto w-7/8 rounded-lg p-6">
-      <div className="flex h-full flex-col items-center space-y-8 rounded-lg border-3 border-white px-2 pt-2 text-center text-white">
-        <Image src={imageSrc} alt={"Image of " + cardTitle} height={70} />
-        <div className="text-2xl font-bold">{cardTitle}</div>
-        <p className="mb-5 text-lg font-thin">{cardText}</p>
-      </div>
-    </div>
-  );
+    return (
+        <motion.div
+            whileHover={{ scale: 1.05 }}
+            className="bg-hcg-pastel-brown-secondary mx-auto w-7/8 rounded-lg p-6"
+        >
+            <div className="flex h-full flex-col items-center space-y-8 rounded-lg border-3 border-white px-2 pt-2 text-center text-white">
+                <Image src={imageSrc} alt={"Image of " + cardTitle} height={70} className="mt-2" />
+                <div className="text-2xl font-bold">{cardTitle}</div>
+                <p className="mb-5 text-lg font-thin">{cardText}</p>
+            </div>
+
+        </motion.div>
+    );
 };
 
 export default SolutionCard;
--- src/components/work/Solutions.tsx
+++ src/components/work/Solutions.tsx
@@ -1,25 +1,39 @@
+"use client"
+import { motion } from "motion/react";
+
+
 import SolutionCard from "@/components/work/SolutionCard";
 import { solutionsInfos } from "@/data/SolutionCards";
 import Title from "@/components/Title";
 
 const Solutions = () => {
-  return (
-    <div className="mx-auto w-5/6">
-      <div className="mb-8">
-        <Title title="Solutions" />
-      </div>
-      <div className="flex flex-col gap-4 md:flex-row md:gap-4">
-        {solutionsInfos.map(({ cardTitle, cardText, imageSrc }, index) => (
-          <SolutionCard
-            key={index}
-            cardTitle={cardTitle}
-            cardText={cardText}
-            imageSrc={imageSrc}
-          />
-        ))}
-      </div>
-    </div>
-  );
+    return (
+        <div className="mx-auto w-5/6">
+            <div className="mb-10">
+                <Title title="Solutions" />
+            </div>
+            <div className="flex flex-col gap-4 md:flex-row md:gap-4">
+                {solutionsInfos.map(({ cardTitle, cardText, imageSrc }, index) => (
+                    <motion.div
+                        key={index}
+                        initial={{ scale: 0 }}
+                        whileInView={{ scale: 1.0 }}
+                        transition={{
+                            duration: 0.3,
+                            delay: index * 0.1
+                        }}
+                        viewport={{once: true}}
+                    >
+                        <SolutionCard
+                            cardTitle={cardTitle}
+                            cardText={cardText}
+                            imageSrc={imageSrc}
+                        />
+                    </motion.div>
+                ))}
+            </div>
+        </div>
+    );
 };
 
 export default Solutions;
