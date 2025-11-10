d130e19f4a660414ec001a595021a6ee959edd43
Kevin Loritsch <kloritsch58@gmail.com>
2025-11-10 11:51:48 -0800

    fix small sizing

--- src/components/work/SolutionCard.tsx
+++ src/components/work/SolutionCard.tsx
@@ -1,7 +1,4 @@
-"use client";
-
 import Image, { StaticImageData } from "next/image";
-import { motion } from "motion/react";
 
 interface SolutionCardProps {
   cardTitle: string;
@@ -11,10 +8,7 @@ interface SolutionCardProps {
 
 const SolutionCard = ({ cardTitle, cardText, imageSrc }: SolutionCardProps) => {
   return (
-    <motion.div
-      whileHover={{ scale: 1.05 }}
-      className="bg-hcg-pastel-brown-secondary mx-auto w-7/8 rounded-lg p-6"
-    >
+    <div className="bg-hcg-pastel-brown-secondary h-full w-full rounded-lg p-6">
       <div className="flex h-full flex-col items-center space-y-8 rounded-lg border-3 border-white px-2 pt-2 text-center text-white">
         <Image
           src={imageSrc}
@@ -25,7 +19,7 @@ const SolutionCard = ({ cardTitle, cardText, imageSrc }: SolutionCardProps) => {
         <div className="text-2xl font-bold">{cardTitle}</div>
         <p className="mb-5 text-lg font-thin">{cardText}</p>
       </div>
-    </motion.div>
+    </div>
   );
 };
 
--- src/components/work/Solutions.tsx
+++ src/components/work/Solutions.tsx
@@ -8,20 +8,22 @@ import Title from "@/components/Title";
 const Solutions = () => {
   return (
     <div className="mx-auto w-5/6">
-      <div className="mb-10">
+      <div className="mb-8">
         <Title title="Solutions" />
       </div>
-      <div className="flex flex-col gap-4 md:flex-row md:gap-4">
+      <div className="mx-auto flex flex-col gap-4 md:flex-row md:gap-6">
         {solutionsInfos.map(({ cardTitle, cardText, imageSrc }, index) => (
           <motion.div
             key={index}
             initial={{ scale: 0 }}
             whileInView={{ scale: 1.0 }}
+            whileHover={{ scale: 1.03 }}
             transition={{
               duration: 0.3,
               delay: index * 0.1,
             }}
             viewport={{ once: true }}
+            className="w-full md:w-1/3"
           >
             <SolutionCard
               cardTitle={cardTitle}
