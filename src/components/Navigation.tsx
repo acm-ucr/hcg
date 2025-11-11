9a023291c3a20818591d8ce9484d66fb22fc3e22
Willow <isaiasnoel@gmail.com>
2025-11-11 08:26:00 -0800

    formatting

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -18,12 +18,12 @@ const Navigation = () => {
 
   return (
     <div>
-      <div className="flex h-24 items-center fixed inset-x-0 top-0 z-[60] from-hcg-black to-hcg-gold bg-linear-to-r justify-between p-8">
+      <div className="from-hcg-black to-hcg-gold fixed inset-x-0 top-0 z-[60] flex h-24 items-center justify-between bg-linear-to-r p-8">
         <div className="flex w-full">
           <Image src={SmallLogo} alt="SmallLogo" />
         </div>
 
-        <div className="space-between text-hcg-white hidden h-auto items-center gap-8 pr-8 text-xl text-nowrap md:inline-flex">
+        <div className="space-between text-hcg-white hidden h-auto items-center gap-8 pr-8 text-xl whitespace-nowrap md:inline-flex">
           {navbarLinks.map(({ name, link }, index) => (
             <Link href={link} key={index}>
               {name}
@@ -31,28 +31,23 @@ const Navigation = () => {
           ))}
         </div>
 
-        <button 
+        <button
           className="text-hcg-white flex text-5xl md:inline lg:hidden"
-          onClick={toggleIconVisibility}>
-          {visibility ? (
-            <HiXMark/>
-          ) : (
-            <FaBars/>
-          )}
+          onClick={toggleIconVisibility}
+        >
+          {visibility ? <HiXMark /> : <FaBars />}
         </button>
       </div>
 
       {/* Mobile Dropdown Below */}
       {visibility && (
-        <div
-          className="fixed inset-0 z-50 flex flex-col items-center justify-start bg-linear-to-r from-hcg-black to-hcg-gold md:hidden text-hcg-white whitespace-nowrap"
-        >
-          <div className="pt-24 w-full max-w-xl px-5 flex flex-col items-stretch gap-8">
+        <div className="from-hcg-black to-hcg-gold text-hcg-white fixed inset-0 z-50 flex flex-col items-center justify-start bg-linear-to-r whitespace-nowrap md:hidden">
+          <div className="flex w-full max-w-xl flex-col items-stretch gap-8 px-5 pt-24">
             {navbarLinks.map(({ name, link }, index) => (
               <Link
                 href={link}
                 key={index}
-                className="w-full text-center text-5xl px-4 py-4"
+                className="w-full px-4 py-4 text-center text-5xl"
                 onClick={toggleIconVisibility}
               >
                 {name}
