5902a9c46f8caade0e71ea06af8c40fc7703c158
Jason <bennguyen20122006@gmail.com>
2025-11-11 18:29:11 -0800

    Completed Alumni Mapping

--- src/components/alumni/AlumniMapping.tsx
+++ src/components/alumni/AlumniMapping.tsx
@@ -1,5 +1,24 @@
+"use client";
+import AlumniCard from "@/components/alumni/AlumniCard";
+import { AlumniData } from "@/data/AlumniData";
+
 const AlumniMapping = () => {
-  return <div>AlumniMapping</div>;
+  return (
+    <div className="flex flex-row flex-wrap items-center justify-center">
+      {AlumniData.map((member, index) => (
+        <div key={index} className="flex w-1/2 justify-center md:w-1/3">
+          <AlumniCard
+            key={index}
+            name={member.name}
+            role={member.role}
+            currentOccupation={member.currentOccupation}
+            image={member.image}
+            linkedin={member.linkedin}
+          />
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default AlumniMapping;
