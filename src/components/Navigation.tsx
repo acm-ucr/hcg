2a66122462391f768fa1a2cd2c8d7540e9f5ef9f
APerson09 <ethanhuang63@gmail.com>
2025-11-16 15:39:33 -0800

    Made changes to NavBar

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -6,60 +6,62 @@ import Image from "next/image";
 import SmallLogo from "@/public/smallLogo.webp";
 import { FaBars } from "react-icons/fa";
 import { HiXMark } from "react-icons/hi2";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const Navigation = () => {
   // const [select, selected] = useState("");
-  const [visibility, toggleVisibility] = useState(true);
+  const [visibility, toggleVisibility] = useState(false);
 
   const toggleIconVisibility = () => {
     toggleVisibility(!visibility);
   };
 
+  useEffect(() => {
+    if (visibility) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = " ";
+    }
+  }, [visibility]);
+
   return (
-    <div className="from-hcg-black to-hcg-gold flex h-24 items-center justify-between bg-linear-to-r p-8">
-      <div className="flex w-full">
-        <Image src={SmallLogo} alt="SmallLogo" />
+    <div>
+      <div className="from-hcg-black to-hcg-gold sticky inset-x-0 top-0 z-[60] flex h-24 items-center justify-between bg-linear-to-r p-2">
+        <div className="flex w-full">
+          <Image src={SmallLogo} alt="SmallLogo" />
+        </div>
+        <div className="space-between text-hcg-white flex hidden h-auto items-center gap-8 pr-8 text-xl text-nowrap md:inline-flex">
+          {navbarLinks.map(({ name, link }, index) => (
+            <Link href={link} key={index}>
+              {name}
+            </Link>
+          ))}
+        </div>
+
+        <button
+          className="text-hcg-white flex text-5xl md:inline lg:hidden"
+          onClick={toggleIconVisibility}
+        >
+          {visibility ? <HiXMark /> : <FaBars />}
+        </button>
       </div>
 
-      <div className="space-between text-hcg-white flex hidden h-auto items-center gap-8 pr-8 text-xl text-nowrap md:inline-flex">
-        {navbarLinks.map(({ name, link }, index) => (
-          <Link href={link} key={index}>
-            {" "}
-            {name}
-          </Link>
-        ))}
-      </div>
-
-      <div
-        className={
-          visibility
-            ? "space-between text-hcg-white fixed top-5 top-[20%] z-10 flex w-[100%] flex-col items-center gap-8 pr-8 text-5xl text-nowrap md:inline-flex lg:hidden"
-            : "hidden"
-        }
-      >
-        {navbarLinks.map(({ name, link }, index) => (
-          <Link
-            href={link}
-            key={index}
-            onClick={() => {
-              // selected(name);
-              toggleIconVisibility();
-            }}
-          >
-            {" "}
-            {name}
-          </Link>
-        ))}
-      </div>
-
-      <button onClick={toggleIconVisibility}>
-        {visibility ? (
-          <HiXMark className="text-hcg-white flex text-5xl md:inline lg:hidden" />
-        ) : (
-          <FaBars className="text-hcg-white flex text-4xl md:inline lg:hidden" />
-        )}
-      </button>
+      {visibility && (
+        <div className="from-hcg-black to-hcg-gold text-hcg-white sticky inset-0 top-0 z-50 flex flex-col items-center justify-start bg-linear-to-r whitespace-nowrap md:hidden">
+          <div className="flex w-full max-w-xl flex-col items-stretch gap-8 pt-24 pb-42">
+            {navbarLinks.map(({ name, link }, index) => (
+              <Link
+                href={link}
+                key={index}
+                className="w-full px-4 py-4 text-center text-5xl"
+                onClick={toggleIconVisibility}
+              >
+                {name}
+              </Link>
+            ))}
+          </div>
+        </div>
+      )}
     </div>
   );
 };
