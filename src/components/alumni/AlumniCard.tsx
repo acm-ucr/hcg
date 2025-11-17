d25986f8015a14e9ffad861cdcea603d61e4eb1c
Tarunvir <gtarunvir@gmail.com>
2025-11-16 19:56:10 -0800

    alumni animation

--- src/components/alumni/AlumniCard.tsx
+++ src/components/alumni/AlumniCard.tsx
@@ -1,3 +1,5 @@
+"use client";
+import { motion } from "motion/react";
 import Image, { StaticImageData } from "next/image";
 import Link from "next/link";
 import { FaLinkedin } from "react-icons/fa";
@@ -26,10 +28,11 @@ const AlumniCard = ({
       <div className="text-hcg-dark-gray mb-2 font-normal">{role}</div>
 
       <div className="text-hcg-dark-gray mb-4">{currentOccupation}</div>
-
-      <Link href={linkedin} target="_blank" className="mt-auto">
-        <FaLinkedin className="text-hcg-gold h-8 w-8" />
-      </Link>
+      <motion.div whileHover={{ scale: 1.1 }}>
+        <Link href={linkedin} target="_blank" className="mt-auto">
+          <FaLinkedin className="text-hcg-gold h-8 w-8" />
+        </Link>
+      </motion.div>
     </div>
   );
 };
--- src/components/alumni/AlumniMapping.tsx
+++ src/components/alumni/AlumniMapping.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { motion } from "motion/react";
 import AlumniCard from "@/components/alumni/AlumniCard";
 import { AlumniData } from "@/data/AlumniData";
 
@@ -6,7 +7,13 @@ const AlumniMapping = () => {
   return (
     <div className="flex flex-row flex-wrap justify-center">
       {AlumniData.map((member, index) => (
-        <div key={index} className="w-1/2 md:w-1/3">
+        <motion.div
+          key={index}
+          initial={{ opacity: 0.75, scale: 0.85 }}
+          whileInView={{ opacity: 1, scale: 1 }}
+          transition={{ duration: 0.6, delay: index * 0.15 }}
+          className="w-1/2 md:w-1/3"
+        >
           <AlumniCard
             key={index}
             name={member.name}
@@ -15,7 +22,7 @@ const AlumniMapping = () => {
             image={member.image}
             linkedin={member.linkedin}
           />
-        </div>
+        </motion.div>
       ))}
     </div>
   );
