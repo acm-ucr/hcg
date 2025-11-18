18f22ceb976b590c644951d000e26715214ec663
Kevin Loritsch <kloritsch58@gmail.com>
2025-11-17 17:49:25 -0800

    minor tweaks

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -16,22 +16,15 @@ const Navigation = () => {
     toggleVisibility(!visibility);
   };
 
-  useEffect(() => {
-    if (visibility) {
-      document.body.style.overflow = "hidden";
-    } else {
-      document.body.style.overflow = " ";
-    }
-  }, [visibility]);
 
   return (
     <div>
-      <div className="from-hcg-black to-hcg-gold sticky inset-x-0 top-0 z-[60] flex h-24 items-center justify-between bg-linear-to-r p-2">
-        <div className="flex w-full">
+      <div className="from-hcg-black to-hcg-gold inset-x-0 fixed top-0 z-[60] flex h-24 items-center justify-between bg-linear-to-r p-2">
+        <div className="flex w-full sticky">
           <Image src={SmallLogo} alt="SmallLogo" />
         </div>
 
-        <div className="space-between text-hcg-white hidden h-auto items-center gap-8 pr-8 text-xl whitespace-nowrap md:inline-flex">
+        <div className="space-between text-hcg-white  hidden h-auto items-center gap-8 pr-8 text-xl md:flex">
           {navbarLinks.map(({ name, link }, index) => (
             <Link href={link} key={index}>
               {name}
@@ -48,21 +41,22 @@ const Navigation = () => {
       </div>
 
       {visibility && (
-        <div className="from-hcg-black to-hcg-gold text-hcg-white sticky inset-0 z-50 flex flex-col items-center justify-start bg-linear-to-r whitespace-nowrap md:hidden">
-          <div className="flex w-full max-w-xl flex-col items-stretch gap-8 pt-24 pb-42">
-            {navbarLinks.map(({ name, link }, index) => (
-              <Link
-                href={link}
-                key={index}
-                className="w-full px-4 py-4 text-center text-5xl"
-                onClick={toggleIconVisibility}
-              >
-                {name}
-              </Link>
-            ))}
-          </div>
-        </div>
-      )}
+  <div className="from-hcg-black to-hcg-gold text-hcg-white fixed top-24 left-0 right-0 z-50 flex flex-col items-center justify-start bg-linear-to-r md:hidden">
+    <div className="flex w-full max-w-xl flex-col gap-8 py-8">
+      {navbarLinks.map(({ name, link }, index) => (
+        <Link
+          href={link}
+          key={index}
+          className="w-full px-4 py-4 text-center text-3xl"
+          onClick={toggleIconVisibility}
+        >
+          {name}
+        </Link>
+      ))}
+    </div>
+  </div>
+)}
+
     </div>
   );
 };
